fdac95039127dd0ee8d0517ca4556d60ece52b5e
khushpatel00 <patelxkhush@gmail.com>
2025-11-11 00:36:43 +0530

    added: active player fixed: input slider, and thumbnail/title placement

--- assets/libs/core.js
+++ assets/libs/core.js
@@ -1,54 +1,61 @@
 "use strict";
-let playerToggler = document.querySelector('.toggler');
-let durationContainer = document.querySelector('.duration');
-let currentPlayer = document.getElementById('currentPlayer');
-let playerContainer = document.getElementById('playerContainer');
-let playerSlider = document.getElementById('playerSlider');
-let title = document.getElementById('title');
+let playerToggler = document.querySelector(".toggler");
+let durationContainer = document.querySelector(".duration");
+let currentPlayer = document.getElementById("currentPlayer");
+let playerContainer = document.getElementById("playerContainer");
+let playerSlider = document.getElementById("playerSlider");
+let audTitle = document.querySelectorAll(".currentTitle");
 
-window.addEventListener('keydown', (e) => { // player toggle
-    if (e.key == ' ') togglePlayer();
+window.addEventListener("keydown", (e) => {
+    if (e.key == " ") togglePlayer();
 });
-playerToggler.textContent = '|>'; // paused
+
+
+// window.addEventListener("keydown", (e) => {
+//     console.log(e.key);
+// });
+
+playerToggler.textContent = "|>"; // paused / default expression
+
 function togglePlayer() {
     if (currentPlayer.paused) {
         currentPlayer.play();
-    }
-    else {
+    } else {
         currentPlayer.pause();
     }
 }
-currentPlayer.addEventListener("pause", ()=>{
-    playerToggler.textContent = '|>'; // paused
-})
-currentPlayer.addEventListener("playing", ()=>{
-    playerToggler.textContent = '||'; // playing
-})
+currentPlayer.addEventListener("pause", () => {
+    playerToggler.textContent = "|>"; // paused
+});
+currentPlayer.addEventListener("playing", () => {
+    playerToggler.textContent = "||"; // playing
+});
 
-setTimeout(function () { // time stamp before played
+setTimeout(function () {
+    // time stamp before played
     durationContainer.innerHTML = `${formatTime(currentPlayer.currentTime)} / ${formatTime(currentPlayer.duration)}`;
-}, 150);
+}, 100); 
 
+durationContainer.innerHTML = `${formatTime(currentPlayer.currentTime)} / ${formatTime(currentPlayer.duration)}`;
 
-function formatTime(time) { // formats time in seconds:minutes format
+function formatTime(time) {
+    // formats time in seconds:minutes format
     const minutes = Math.floor(time / 60);
     const seconds = Math.floor(time % 60);
-    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
 }
 
-playerSlider.style.background = `linear-gradient(90deg, #ca3500 ${((playerSlider.value / playerSlider.max) * 100)}%, #ddd ${((playerSlider.value / playerSlider.max) * 100)}%)`;
-// playerSlider.max = Math.floor(currentPlayer.duration);
-// playerSlider.value = 20;
-// playerSlider.max = Math.floor(currentPlayer.duration);
+// default lookup, for exception: no audio file mounted
+playerSlider.style.background = `linear-gradient(90deg, #ca3500 ${(playerSlider.value / playerSlider.max) * 100}%, #ddd ${(playerSlider.value / playerSlider.max) * 100}%)`;
 
-currentPlayer.addEventListener('timeupdate', () => { // timestamp while being played (live)
-    playerSlider.style.background = `linear-gradient(90deg, #ca3500 ${((playerSlider.value / playerSlider.max) * 100)}%, #ddd ${((playerSlider.value / playerSlider.max) * 100)}%)`;
+currentPlayer.addEventListener("timeupdate", () => {
+    // timestamp while being played (live)
+    playerSlider.style.background = `linear-gradient(90deg, #ca3500 ${(playerSlider.value / playerSlider.max) * 100}%, #ddd ${(playerSlider.value / playerSlider.max) * 100}%)`;
     durationContainer.innerHTML = `${formatTime(currentPlayer.currentTime)} / ${formatTime(currentPlayer.duration)}`;
-    playerSlider.value = Math.floor(currentPlayer.currentTime)
+    playerSlider.value = Math.floor(currentPlayer.currentTime);
     playerSlider.max = Math.floor(currentPlayer.duration);
 });
-playerSlider.addEventListener('input', () => {
+playerSlider.addEventListener("input", () => {
     currentPlayer.currentTime = playerSlider.value;
     playerSlider.max = Math.floor(currentPlayer.duration);
-}); 
-
+});
--- assets/libs/script.js
+++ assets/libs/script.js
@@ -1,19 +1,21 @@
 
 fileName = currentPlayer.src.split('/').pop().split('.').slice(0, -1).toString().replaceAll("%20", " ");
 if(fileName.length > 30) fileName = fileName.slice(0, 30) + ' . . .';
-title.innerText = fileName;
-
+for (let index = 0; index < audTitle.length; index++) {
+  audTitle[index].innerHTML = fileName;
+};
 
 jsmediatags.read(currentPlayer.currentSrc, {
-  onSuccess: tag => {
-    const picture = tag.tags.picture;
-    if (picture) {
-      const base64String = picture.data.map(b => String.fromCharCode(b)).join('');
-      const imageUrl = `data:${picture.format};base64,${btoa(base64String)}`;
-      document.getElementById('thumb').src = imageUrl;
-    } else {
-      console.log("No album art found");
-    }
-  },
-  onError: err => console.error(err)
+ onSuccess: tag => {
+   const picture = tag.tags.picture;
+   if (picture) {
+     const base64String = picture.data.map(b => String.fromCharCode(b)).join('');
+     const imageUrl = `data:${picture.format};base64,${btoa(base64String)}`;
+     document.getElementsByClassName('thumb')[0].src = imageUrl;
+     document.getElementsByClassName('thumb')[1].src = imageUrl;
+   } else {
+     console.log("No album art found");
+   }
+ },
+ onError: err => console.error(err)
 });
